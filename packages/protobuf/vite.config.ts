73c02e17130632d38f6110727d1b4feb3b71db2f
Andrew Mello <andrew@88plug.com>
2025-11-16 11:41:37 -0800

    chore: Fix vite config to generate index.js and index.cjs

--- packages/protobuf/vite.config.ts
+++ packages/protobuf/vite.config.ts
@@ -48,6 +48,7 @@ export default defineConfig({
     lib: {
       entry: 'src/index.ts',
       name: 'AkashProtobuf',
+      fileName: (format) => format === 'es' ? 'index.js' : 'index.cjs',
       formats: ['es', 'cjs']
     },
     rollupOptions: {
